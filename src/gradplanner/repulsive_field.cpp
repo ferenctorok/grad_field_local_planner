78749a0d4fa397d601905dc3d59de73f510a4b3a
Ferenc Török <ferenc.toeroek@iteratec.com>
2021-02-22 10:11:00 +0100

    Setup that works kind of normally. At very narrow places it is zig-zaging, but otherways it is ok.

--- src/gradplanner/repulsive_field.cpp
+++ src/gradplanner/repulsive_field.cpp
@@ -103,11 +103,11 @@ namespace gradplanner
     // decrease linearly from the edge of an obstacle.
     // It has got length 1 at the boarder of an obstacle,
     // that is, in pixels with value 2.
-    double scale1 = (1 - (new_pix->get_val() - 2) / R);
-    double scale2 = sqrt((1 - pow((new_pix->get_val() - 2) / R, 2)));
-    double scale3 = 2 * scale1 - scale2;
-    int val = new_pix->get_val();
-    new_pix->scale_grad(scale3);
+    double dist_ratio = (new_pix->get_val() - 2) / R;
+    //double scale = (1 - dist_ratio);
+    //double scale = sqrt((1 - pow(dist_ratio, 2)));
+    double scale = 1 - sqrt((1 - pow((1 - dist_ratio), 2)));
+    new_pix->scale_grad(scale);
   }
 
   bool RepulsiveField::is_special_case()
